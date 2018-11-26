e050cb6ee4d3a6588160a08c035b7565ae95d326
lccasagrande <lcamelocasagrande@gmail.com>
2018-11-25 22:19:42 -0200

    Change init to new name

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from gym.envs.registration import register
 
 register(
-    id='grid-v0',
-    entry_point='gym_grid.envs:GridEnv',
+	id='grid-v0',
+	entry_point='GridEnv.envs:GridEnv',
 )
 
 register(
-    id='batsim-v0',
-    entry_point='gym_grid.envs:BatsimEnv',
+	id='batsim-v0',
+	entry_point='GridEnv.envs:BatsimEnv',
 )
