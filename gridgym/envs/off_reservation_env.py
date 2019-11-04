c6afe771044629d6f91c349be3ca4cfa21e373b4
lccasagrande <lcamelocasagrande@gmail.com>
2019-11-04 19:56:57 -0300

    Fix incorrect commit of OR env

--- gridgym/envs/off_reservation_env.py
+++ gridgym/envs/off_reservation_env.py
@@ -9,76 +9,9 @@ from gym import spaces
 
 
 from gridgym.envs.grid_env import GridEnv
-from batsim_py.rjms import RJMSHandler
 from batsim_py.utils.schedulers import EASYBackfilling, SAFBackfilling
 from batsim_py.resource import ResourceState, PowerStateType
 from batsim_py.utils.monitors import *
-
-
-class RJMSWrapper(RJMSHandler):
-    def __init__(self, max_queue_sz=None, use_batsim=False):
-        super().__init__(use_batsim=use_batsim)
-        if max_queue_sz:
-            assert max_queue_sz > 0
-
-        self.nb_nodes = -1
-        self.max_queue_sz = max_queue_sz
-
-    @property
-    def jobs_queue(self):
-        queue = super().jobs_queue
-        s_queue = [queue[0]]
-        for j in sorted(queue[1:], key=lambda j: j.walltime * j.res):
-            if self.max_queue_sz and len(s_queue) == max_queue_sz:
-                break
-            s_queue.append(j)
-        return np.asarray(s_queue)
-
-    def on_simulation_begins(self, timestamp, data):
-        super().on_simulation_begins(timestamp, data)
-        self.nb_nodes = self.platform.nb_nodes
-
-    def set_platform_size(self, sz):
-        assert 0 <= self.sz <= self.platform.nb_nodes, 'Cannot expand beyond platform size'
-        self.nb_nodes = sz
-
-    def get_available_resources(self, include_omitted=False):
-        res = super().get_available_resources()
-        res = sorted(res, key=lambda r: r.state.value)
-        if include_omitted:
-            return res
-
-        nb_res = self.nb_nodes * self.rjms.platform.nodes[0].nb_resources
-        return res[:nb_res]
-
-    def get_available_nodes(self, include_omitted=False):
-        nodes = super().get_available_nodes()
-        nodes = sorted(nodes, key=lambda n: max(
-            r.state.value for r in n.resources))
-        if include_omitted:
-            return nodes
-
-        return nodes[:self.nb_nodes]
-
-    def get_reserved_time(self, include_omitted=False):
-        reserved = super().get_reserved_time()
-        reserved.sort()
-        if include_omitted:
-            return reserved
-
-        nb_res = self.nb_nodes * \
-            self.rjms.platform.nodes[0].nb_resources  # Temp fix
-        return reserved[nb_res:]
-
-    def get_progress(self, include_omitted=False):
-        reserved = super().get_progress()
-        reserved.sort()
-        if include_omitted:
-            return reserved
-
-        nb_res = self.nb_nodes * \
-            self.rjms.platform.nodes[0].nb_resources  # Temp fix
-        return reserved[nb_res:]
 
 
 class OffReservationEnv(GridEnv):
@@ -93,6 +26,7 @@ class OffReservationEnv(GridEnv):
 
         self.act_interval = act_interval
         self.max_queue_sz = max_queue_sz
+        self.reservation_size = 0
 
         super().__init__(
             use_batsim=use_batsim,
@@ -101,17 +35,27 @@ class OffReservationEnv(GridEnv):
             export=export,
             qos_stretch=qos_stretch)
 
-        self.scheduler = EASYBackfilling(self.rjms)
+        self.scheduler = EASYBackfilling()
+        self.metadata['render.modes'] = []
 
-    def _get_rjms(self, use_batsim):
-        return RJMSWrapper(self.max_queue_sz, use_batsim)
+    def reset(self):
+        self.reservation_size = 0
+        obs = super().reset()
+        return obs
 
     def step(self, action):
         assert self.rjms.is_running, "Simulation is not running."
 
         self._set_off_reservation_size(action)
 
-        jobs_to_start = self.scheduler.schedule()
+        reserved = self.rjms.get_reserved_time()
+        reserved.sort()
+        jobs_to_start = self.scheduler.schedule(
+            self._get_queue(self.max_queue_sz),
+            reserved[self.reservation_size * self.rjms.platform.nodes[0].nb_resources:])
+
+        for job_id in jobs_to_start:
+            self.rjms.allocate(job_id)
 
         self.rjms.start_ready_jobs()
 
@@ -127,31 +71,42 @@ class OffReservationEnv(GridEnv):
         info = self._get_info()
         return obs, reward, done, info
 
+    def _get_queue(self, maxlen=0):
+        assert maxlen >= 0
+        s_queue = []
+        if self.rjms.queue_lenght > 0:
+            queue = self.rjms.jobs_queue
+            s_queue.append(queue[0])
+            for j in sorted(queue[1:], key=lambda j: j.walltime * j.res):
+                if len(s_queue) == maxlen:
+                    break
+                s_queue.append(j)
+        return s_queue
+
     def _set_off_reservation_size(self, size):
-        if size != 0:
-            # delimit the reservation size
-            nodes_available = self.rjms.get_available_nodes(
-                include_omitted=True)
-            size = min(size, len(nodes_available))
+        if size == 0:
+            self.reservation_size = 0
+            return
 
-            # Just update the scheduler to see if we delay the priority job
-            pjob = self.rjms.jobs_queue[0] if self.rjms.queue_lenght > 0 else None
-            # free some resources for priority job
-            if pjob and pjob.expected_time_to_start == 0:
-                nb_nodes_needed = math.ceil(
-                    pjob.res / self.rjms.platform.nodes[0].nb_resources)
-                nb_nodes_free = self.rjms.platform.nb_nodes - nb_nodes_needed
-                size = min(size, nb_nodes_free)
+        # delimit the reservation size
+        nodes_available = self.rjms.get_available_nodes()
+        self.reservation_size = min(size, len(nodes_available))
 
-            # find how many nodes must be turned off
-            nodes_on = [n.id for n in nodes_available if n.is_on]
-            if nodes_on:
-                nb_not_on = len(nodes_available) - len(nodes_on)
-                nb_to_turn_off = max(0, size - nb_not_on)
-                self.rjms.turn_off(*nodes_on[:nb_to_turn_off])
+        # Just update the scheduler to see if we delay the priority job
+        pjob = self.rjms.jobs_queue[0] if self.rjms.queue_lenght > 0 else None
+        # free some resources for priority job
+        if pjob and pjob.expected_time_to_start == 0:
+            nb_nodes_needed = math.ceil(
+                pjob.res / self.rjms.platform.nodes[0].nb_resources)
+            nb_nodes_free = self.rjms.platform.nb_nodes - nb_nodes_needed
+            self.reservation_size = min(self.reservation_size, nb_nodes_free)
 
-        size = self.rjms.platform.nb_nodes - size
-        self.rjms.set_platform_size(size)
+        # find how many nodes must be turned off
+        nodes_on = [n.id for n in nodes_available if n.is_on]
+        if nodes_on:
+            nb_not_on = len(nodes_available) - len(nodes_on)
+            nb_to_turn_off = max(0, self.reservation_size - nb_not_on)
+            self.rjms.turn_off(*nodes_on[:nb_to_turn_off])
 
     def _get_reward(self):
         energy_waste = 0
@@ -161,29 +116,32 @@ class OffReservationEnv(GridEnv):
                 energy_waste += n.power / p_max
         energy_waste /= self.rjms.platform.nb_nodes
 
-        queue, qos = self.rjms.jobs_queue, 0
-        if self.rjms.nb_nodes != self.rjms.platform.nb_nodes and len(queue) > 0:
-            agenda = self.rjms.get_reserved_time(include_omitted=True)
-            resources = self.rjms.get_available_resources(include_omitted=True)
-            scheduler_plan = self.scheduler.plan(queue, resources, agenda)
-            for job, alloc in scheduler_plan:
+        queue = self._get_queue(self.max_queue_sz)
+        qos = 0
+        if self.reservation_size != 0 and len(queue) > 0:
+            r = self.rjms.get_reserved_time()
+            jobs_ready = self.scheduler.schedule(queue, r)
+            for job_id in jobs_ready:
+                job = next(j for j in queue if j.id == job_id)
                 if (self.rjms.current_time - job.subtime) / job.walltime >= self.qos_stretch:
                     qos += job.res
             qos /= self.rjms.platform.nb_resources
-
         return -1 * (energy_waste + qos)
 
     def _get_obs(self, reward=0):
         obs = {}
 
         # Update scheduler
-        agenda = self.rjms.get_reserved_time()
-        resources = self.rjms.get_available_resources()
-        self.scheduler.plan(self.rjms.jobs_queue, resources, agenda)
+        reserved = self.rjms.get_reserved_time()
+        reserved.sort()
+        nb_reserved = self.reservation_size * \
+            self.rjms.platform.nodes[0].nb_resources
+        self.scheduler.schedule(self._get_queue(
+            self.max_queue_sz), reserved[nb_reserved:])
 
         obs['queue'] = np.asarray(
             [
-                [j.subtime, j.res, j.walltime, j.expected_time_to_start, j.user, int(j.profile)] for j in self.rjms.jobs_queue
+                [j.subtime, j.res, j.walltime, j.expected_time_to_start, j.user, int(j.profile)] for j in self._get_queue()
             ],
             dtype=self.observation_space.spaces['queue'].dtype
         )
@@ -205,10 +163,10 @@ class OffReservationEnv(GridEnv):
             shape=self.observation_space.spaces['agenda'].shape,
             dtype=self.observation_space.spaces['agenda'].dtype)
 
-        for i, p in enumerate(self.rjms.get_progress(include_omitted=True)):
+        for i, p in enumerate(self.rjms.get_progress()):
             obs['agenda'][i] = p
 
-        obs['reservation_size'] = self.rjms.platform.nb_nodes - self.rjms.nb_nodes
+        obs['reservation_size'] = self.reservation_size
         obs['time'] = self.rjms.current_time
         obs['reward'] = reward
         return obs
