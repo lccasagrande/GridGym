684cce070de93594e5b391b6799642380524606a
lccasagrande <lcamelocasagrande@gmail.com>
2019-11-04 20:23:31 -0300

    Fix scheduling env and example.

---
+++ examples/scheduling.py
@@ -0,0 +1,35 @@
+import argparse
+
+import gym
+import gridgym.envs.off_reservation_env as e
+from batsim_py.utils.graphics import plot_simulation_graphics
+
+def run(args):
+    print("[RUNNING]")
+
+    env = gym.make(args.env_id, export=True, max_queue_sz=args.queue_sz)
+
+    obs, done, score = env.reset(), False, 0
+    while not done:
+        obs, reward, done, info = env.step(1)
+        score += reward
+
+    print("[DONE] Score: {} - Output: /tmp/GridGym/{}".format(score, info['workload_name']))
+
+    if args.plot_results:
+        plot_simulation_graphics(
+            "/tmp/GridGym/{}".format(info['workload_name']), show=True)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env_id", default="Scheduling-v0", type=str)
+    parser.add_argument("--plot_results", default=True, action="store_true")
+
+    # Agent specific args
+    parser.add_argument("--queue_sz", default=20, type=int)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    run(parse_args())
--- gridgym/envs/scheduling_env.py
+++ gridgym/envs/scheduling_env.py
@@ -11,9 +11,25 @@ from batsim_py.resource import ResourceState, PowerStateType
 
 
 class SchedulingEnv(GridEnv):
-    MAX_QUEUE_SZ = 20
-    ACT_INTERVAL = 1  # minutes
     SIMULATION_TIME = None  # 60 * 60 * 24  # minutes
+
+    def __init__(self,
+                 use_batsim=False,
+                 simulation_time=1440,
+                 files_dir=None,
+                 export=False,
+                 max_queue_sz=20,
+                 act_interval=1,
+                 qos_stretch=None):
+
+        self.act_interval = act_interval
+        self.max_queue_sz = max_queue_sz
+        super().__init__(
+            use_batsim=use_batsim,
+            simulation_time=simulation_time,
+            files_dir=files_dir,
+            export=export,
+            qos_stretch=qos_stretch)
 
     def step(self, action):
         assert self.rjms.is_running, "Simulation is not running."
@@ -23,83 +39,94 @@ class SchedulingEnv(GridEnv):
                 self.rjms.allocate(self.rjms.jobs_queue[action-1].id)
             except:
                 self.rjms.proceed_time(
-                    self.rjms.current_time + self.ACT_INTERVAL)
+                    self.rjms.current_time + self.act_interval)
         else:
-            self.rjms.proceed_time(self.rjms.current_time + self.ACT_INTERVAL)
+            self.rjms.proceed_time(self.rjms.current_time + self.act_interval)
 
         obs = self._get_obs()
         reward = self._get_reward()
         done = not self.rjms.is_running
-        #info = {
-        #    k: v for k, v in self.scheduler_monitor.info.items()
-       # } if done else {}
-        return obs, reward, done, {}
+        info = self._get_info()
+        return obs, reward, done, info
 
     def _get_reward(self):
         penalty = sum(1 / (j.walltime / 2.)
-                      for j in self.rjms.jobs_queue[:self.MAX_QUEUE_SZ])
+                      for j in self.rjms.jobs_queue[:self.max_queue_sz])
         return -1 * penalty
 
     def _get_obs(self):
         obs = {}
-        obs['queue'] = np.full(
-            fill_value=-1,
-            shape=self.observation_space.spaces['queue'].shape,
-            dtype=self.observation_space.spaces['queue'].dtype)
-        for i, j in enumerate(self.rjms.jobs_queue[:self.MAX_QUEUE_SZ]):
-            time_to_start = -1 if j.expected_time_to_start == - \
-                1 else max(0, j.expected_time_to_start - self.ACT_INTERVAL)
-            obs['queue'][i] = np.asarray(
-                [j.subtime, j.res, j.walltime, time_to_start])
+
+        # Update scheduler
+        obs['queue'] = np.asarray(
+            [
+                [j.subtime, j.res, j.walltime, j.expected_time_to_start, j.user, int(j.profile)] for j in self.rjms.jobs_queue[:self.max_queue_sz]
+            ],
+            dtype=self.observation_space.spaces['queue'].dtype
+        )
+
+        obs['jobs_running'] = np.asarray(
+            [
+                [j.start_time, j.res, j.walltime, j.user, int(j.profile)] for j in self.rjms.jobs_running
+            ]
+        )
 
         obs['platform'] = np.zeros(
             shape=self.observation_space.spaces['platform'].shape,
             dtype=self.observation_space.spaces['platform'].dtype)
-        for i, n in enumerate(self.rjms.platform.nodes):
-            obs['platform'][i] = np.asarray(
-                [r.state.value for r in n.resources])
+
+        for n in self.rjms.platform.nodes:
+            obs['platform'][int(n.id)] = [r.state.value for r in n.resources]
 
         obs['agenda'] = np.zeros(
             shape=self.observation_space.spaces['agenda'].shape,
             dtype=self.observation_space.spaces['agenda'].dtype)
 
-        for i, r in enumerate(self.rjms.platform.resources):
-            if r.is_computing:
-                obs['agenda'][i] = np.asarray(
-                    [r.allocated_job.start_time, r.allocated_job.start_time + r.allocated_job.walltime])
-            elif r.is_reserved:
-                obs['agenda'][i] = np.asarray(
-                    [self.rjms.current_time, self.rjms.current_time + r.allocated_job.walltime])
+        for i, p in enumerate(self.rjms.get_progress()):
+            obs['agenda'][i] = p
 
         obs['time'] = self.rjms.current_time
         return obs
 
     def _get_space(self):
-        self.rjms.start(self.PLATFORM, self.workloads[0],
-                        self.OUTPUT, self.SIMULATION_TIME)
+        self._start_simulation()
 
         queue = spaces.Box(
             low=-1,
             high=np.iinfo(int).max,
-            shape=(self.MAX_QUEUE_SZ, 4),
+            shape=(self.max_queue_sz, 6),
             dtype=np.int)
+
+        jobs_running = spaces.Box(
+            low=-1,
+            high=np.iinfo(int).max,
+            shape=(self.rjms.platform.nb_resources, 5),
+            dtype=np.int)
+
         platform = spaces.Box(
             low=0,
-            high=np.iinfo(int).max,
-            shape=(self.rjms.platform.nb_nodes, max(
-                n.nb_resources for n in self.rjms.platform.nodes)),
-            dtype=np.int)
+            high=5,
+            shape=(self.rjms.platform.nb_nodes,
+                   self.rjms.platform.nodes[0].nb_resources),
+            dtype=np.float)
+
         agenda = spaces.Box(
             low=0,
-            high=np.iinfo(int).max,
-            shape=(self.rjms.platform.nb_resources, 2),
-            dtype=np.int)
+            high=1,
+            shape=(self.rjms.platform.nb_resources,),
+            dtype=np.float)
+
         obs_space = spaces.Dict({
             'queue': queue,
+            'jobs_running': queue,
             'platform': platform,
             'agenda': agenda,
             'time': spaces.Box(low=0, high=np.iinfo(int).max, shape=(), dtype=np.int)
         })
-        act_space = spaces.Discrete(self.MAX_QUEUE_SZ + 1)
+        act_space = spaces.Discrete(self.rjms.platform.nb_nodes + 1)
         self.rjms.close()
         return obs_space, act_space
+
+    def _get_info(self):
+        info = {'workload_name': self.workload_name}
+        return info
