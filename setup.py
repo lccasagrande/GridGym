a8821f80b717b7bae58afa30b1ba34939dded173
D2 <lcamelocasagrande@gmail.com>
2020-05-29 13:52:34 -0300

    Add dev reqs.

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ with open("gridgym/__version__.py") as version_file:
 
 
 install_requires = read_requirements_file('requirements.txt')
+tests_requires = read_requirements_file('requirements-dev.txt')
+dev_requires = read_requirements_file('requirements-dev.txt')
+setup_requires = ['pytest-runner']
 
 
 setup(
@@ -26,6 +29,11 @@ setup(
     description="An OpenAI Gym environment for resource and job management problems.",
     python_requires='>=3.8',
     install_requires=install_requires,
+    tests_require=tests_requires,
+    setup_requires=setup_requires,
+    extras_require={
+        'dev': dev_requires,
+    },
     packages=find_packages(),
     package_dir={'gridgym': 'gridgym'},
 )
