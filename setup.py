84b3d9e8faae35097981d76005c55e98061cd514
D2 <lcamelocasagrande@gmail.com>
2020-05-29 13:47:40 -0300

    Export requirements and add version.

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-from setuptools import setup
+from os import path
+
+from setuptools import find_packages, setup
 
 
-setup(name='GridGym',
-      author='lccasagrande',
-      version='0.0.1',
-      python_requires='>=3.6',
-      install_requires=[
-              'gym',
-              'numpy',
-              'pandas'
-      ])
+def read_requirements_file(filename):
+    file = '%s/%s' % (path.dirname(path.realpath(__file__)), filename)
+    with open(file) as f:
+        return [line.strip() for line in f]
+
+
+with open("gridgym/__version__.py") as version_file:
+    exec(version_file.read())
+
+
+install_requires = read_requirements_file('requirements.txt')
+
+
+setup(
+    name='GridGym',
+    version=__version__,
+    author='lccasagrande',
+    author_email='lcamelocasagrande@gmail.com',
+    url='https://github.com/lccasagrande/gridgym',
+    description="An OpenAI Gym environment for resource and job management problems.",
+    python_requires='>=3.8',
+    install_requires=install_requires,
+    packages=find_packages(),
+    package_dir={'gridgym': 'gridgym'},
+
+)
