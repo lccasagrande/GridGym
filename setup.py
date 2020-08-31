97a00e5a4aa75daeaab82a764d48c74e1d12569c
D2 <lcamelocasagrande@gmail.com>
2020-08-31 10:32:59 -0300

    Refactoring

--- setup.py
+++ setup.py
@@ -16,9 +16,6 @@ with open("README.rst") as readme_file:
     long_description = readme_file.read()
 
 install_requires = read_requirements_file('requirements.txt')
-tests_requires = read_requirements_file('requirements-dev.txt')
-dev_requires = read_requirements_file('requirements-dev.txt')
-setup_requires = ['pytest-runner']
 
 
 setup(
@@ -28,16 +25,21 @@ setup(
     author='lccasagrande',
     author_email='lcamelocasagrande@gmail.com',
     url='https://github.com/lccasagrande/gridgym',
-    description="An OpenAI Gym environment for resource and job management problems.",
+    download_url=f'https://github.com/lccasagrande/gridgym/archive/v{__version__}.tar.gz',
+    description="An OpenAI Gym environment for resource and job management systems.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     python_requires='>=3.8',
     install_requires=install_requires,
-    tests_require=tests_requires,
-    setup_requires=setup_requires,
-    extras_require={
-        'dev': dev_requires,
-    },
     packages=find_packages(),
     package_dir={'gridgym': 'gridgym'},
+    keywords=["Reinforcement Learning", "Scheduler", "Resource and Job Management", "Cluster"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Clustering",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
